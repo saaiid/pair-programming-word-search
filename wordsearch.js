f1bc33002f36530250a211ea55a2853f62e56c13
saaiid <ibrosakid@gmail.com>
2023-01-19 22:44:22 -0500

    wordSearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
+/* const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+}
+
+module.exports = wordSearch */
+
+const wordSearch = (letters, word) => { 
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    const verticalArr = [];
+    let checkString = ""
+    for (let i = 0; i < letters[0].length; i++) {
+        for (let j = 0; j < letters.length; j++) {
+            checkString += letters[j][i];
+        }
+        if (checkString.includes(word)) {
+            return true;
+        }
+        checkString = ""
+    }
+    return false;
 }
 
 module.exports = wordSearch
